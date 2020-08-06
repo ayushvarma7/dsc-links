e2a66871092839b5459da7c4f719b159b1c6fb23
standmarsh <agrawalshagun07@gmail.com>
2020-08-06 22:31:08 +0530

    more protected routes

--- routes/posts.js
+++ routes/posts.js
@@ -7,12 +7,21 @@ const {
   updatePost,
   uploadPhoto,
 } = require("../controllers/posts");
+const { protect } = require("../middleware/auth");
+
 const router = express.Router();
 
-router.route("/").get(getPosts).post(createPost);
+router.route("/").get(getPosts).post(protect, createPost);
 
-router.route("/:id").get(getPost).delete(deletePost).put(updatePost);
+router
+  .route("/:id")
+  .get(getPost)
+  .delete(protect, deletePost)
+  .put(protect, updatePost);
 
-router.route("/:id/photo").put(uploadPhoto);
+router.route("/:id/photo").put(protect, uploadPhoto);
+
+//TODO Add route to get all posts of the logged in user
+
 
 module.exports = router;
