ed913a7ecde77caf1ac86bc29fa624d24bbc8e9f
standmarsh <agrawalshagun07@gmail.com>
2020-08-06 19:57:47 +0530

    adding initial user setup

--- server.js
+++ server.js
@@ -10,32 +10,39 @@ const path = require("path");
 //Loading config
 dotenv.config({ path: "./config/config.env" });
 
+//Connecting to Database
 connectDB();
 
 //Routing
 const posts = require("./routes/posts");
+const auth = require("./routes/auth");
+
 
 const app = express();
 
 //Body parsing for Json
 app.use(express.json());
 
+//File uploader middleware
 app.use(fileupload());
-
 
 //Morgan
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
+//Setting static folder
+app.use(express.static(path.join(__dirname, "public")));
+
 //Mount routers
 app.use("/api/v1/posts", posts);
+app.use("/api/v1/auth", auth);
 
-app.use(express.static(path.join(__dirname, "public")));
 
 //Custom Error Handling Part
 app.use(errorHandler);
 
+//Port number settings up
 const PORT = process.env.PORT || 5000;
 
 const server = app.listen(
